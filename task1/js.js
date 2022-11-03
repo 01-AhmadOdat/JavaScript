a888d6fb16a4f429bf4d0c14d0cb0e72ee070626
01-AhmadOdat <adodat91@gmail.com>
2022-11-03 15:38:27 +0200

    day1

---
+++ task1/js.js
@@ -0,0 +1,23 @@
+let mark= 55;
+if( mark > 0  & mark < 50 ){
+    document.write("Faild");
+}
+else{
+    if(mark >=50 & mark <60)
+    document.write("D")
+}    
+   
+if( mark >=60  & mark < 70 ){
+    document.write("C");
+}
+else{
+    if(mark >=70 & mark <80)
+    document.write("B")
+}
+if( mark >=80  & mark < 90 ){
+    document.write("A");
+}
+else{
+    if(mark >=90 & mark <=100)
+    document.write("A+")
+}
---
+++ task2/js.js
@@ -0,0 +1,4 @@
+const x = 3.14;
+let r = 2;
+let area = x*r*r;
+document.write(area)
